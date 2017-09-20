84bc03d91aead13d821bf091f51cd853c061ed03
Chris Droukas <c.droukas@gmail.com>
2017-09-20 11:48:32 -0400

    Alignment and visibility together

--- src/Box.js
+++ src/Box.js
@@ -1,11 +1,15 @@
 import styled from 'styled-components'
 import { space, width, color } from 'styled-system'
 import PropTypes from 'prop-types'
+const align = responsiveStyle('text-align', 'align')
+const visibility = responsiveStyle('visibility', 'visibility')
 
 const Box = styled.div`
   ${space}
   ${width}
   ${color}
+  ${align}
+  ${visibility}
 `
 
 Box.displayName = 'Box'
